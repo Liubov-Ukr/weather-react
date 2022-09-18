622568a488328d4f7594160e892b63a798138369
Liubov-Ukr <liubovopr@gmail.com>
2022-09-18 19:38:18 -0200

    fixed warnings

--- src/Footer.js
+++ src/Footer.js
@@ -2,7 +2,11 @@ export default function Footer() {
   return (
     <footer>
       This project was coded by <b>Liubov Opryshchenko</b> and is{" "}
-      <a href="https://github.com/Liubov-Ukr/weather-react" target="_blank">
+      <a
+        href="https://github.com/Liubov-Ukr/weather-react"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
         open-sourced on GitHub
       </a>
     </footer>
--- src/Header.js
+++ src/Header.js
@@ -4,13 +4,11 @@ export default function Header() {
   return (
     <div className="row Header">
       <div className="col-sm-3">
-        <a href="#">
-          <img
-            src="https://i.pinimg.com/originals/77/0b/80/770b805d5c99c7931366c2e84e88f251.png"
-            className="img-thumbnail"
-            alt="logo"
-          />
-        </a>
+        <img
+          src="https://i.pinimg.com/originals/77/0b/80/770b805d5c99c7931366c2e84e88f251.png"
+          className="img-thumbnail"
+          alt="logo"
+        />
       </div>
       <Search />
     </div>
