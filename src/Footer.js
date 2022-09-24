5df4b5bbc5fde865538678bf57422b852fe3a8a3
Liubov-Ukr <liubovopr@gmail.com>
2022-09-24 11:04:12 -0200

    Added target="_blank"

--- src/Footer.js
+++ src/Footer.js
@@ -2,7 +2,14 @@ export default function Footer() {
   return (
     <footer className="mt-5">
       This project was coded by{" "}
-      <a href="https://github.com/Liubov-Ukr">Liubov Opryshchenko</a> and is{" "}
+      <a
+        href="https://github.com/Liubov-Ukr"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        Liubov Opryshchenko
+      </a>{" "}
+      and is{" "}
       <a
         href="https://github.com/Liubov-Ukr/weather-react"
         target="_blank"
