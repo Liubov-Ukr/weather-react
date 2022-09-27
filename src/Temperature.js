3a7297535911500dd1f6476444ec0ffd2e077dda
Liubov-Ukr <liubovopr@gmail.com>
2022-09-27 14:04:48 -0200

    deleted C | F

--- src/Temperature.js
+++ src/Temperature.js
@@ -1,13 +1,7 @@
-import React, { useState } from "react";
+import React from "react";
 import "./Temperature.css";
 
 export default function Temperature(props) {
-  const [unit, setUnit] = useState("celsius");
-  function showfahrenheit(event) {
-    event.preventDefault();
-    setUnit("fahrenheit");
-  }
-
   return (
     <span className="todayTemp">
       {Math.round(props.temp)} <span className="celsius">C°</span>
